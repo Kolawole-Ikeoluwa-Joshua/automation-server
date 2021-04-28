dd9544987370b409c9b824ab92af664af60d0555
Kolawole Ikeoluwa <kolawoleikeoluwa2018@gmail.com>
2021-04-28 19:47:19 +0100

    fix bug in db-script

--- db-script.sh
+++ db-script.sh
@@ -14,7 +14,7 @@ BUCKET_NAME=$6
 
 
 mysqldump --column-statistics=0 -u root -h $DB_HOST -p$DB_PASSWORD $DB_NAME > /tmp/$BACKUP && \
-export AWS_ACCESS_KEY_ID=&AWS_ACCESS && \
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS && \
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET && \
 echo "Uploading your $BACKUP backup" && \
 aws s3 cp /tmp/db-$DATE.sql s3://$BUCKET_NAME/$BACKUP
