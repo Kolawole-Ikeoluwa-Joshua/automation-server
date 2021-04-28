5eb715186afea7b87d1a437ba1eef3a23d36bac8
Kolawole Ikeoluwa <kolawoleikeoluwa2018@gmail.com>
2021-04-28 20:12:23 +0100

    fix bug 2.0

--- db-script.sh
+++ db-script.sh
@@ -5,17 +5,16 @@ DATE=$(date +%H-%M-%S)
 
 BACKUP=db-$DATE.sql
 
-DB_HOST=$1
-DB_PASSWORD=$2
+DB_PASSWORD=$1
+DB_HOST=$2
 DB_NAME=$3
 AWS_ACCESS=$4
 AWS_SECRET=$5
 BUCKET_NAME=$6
 
 
-mysqldump --column-statistics=0 -u root -h $DB_HOST -p$DB_PASSWORD $DB_NAME > /tmp/$BACKUP && \
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS && \
+mysqldump --column-statistics=0 -u root $DB_PASSWORD -h $DB_HOST $DB_NAME > /tmp/$BACKUP && \
+export AWS_ACCESS_KEY_ID=&AWS_ACCESS && \
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET && \
 echo "Uploading your $BACKUP backup" && \
 aws s3 cp /tmp/db-$DATE.sql s3://$BUCKET_NAME/$BACKUP
-
